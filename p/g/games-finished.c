9fe9bc6aff2307f14bee922b84824ab157d337f3
Robin Hack <hack.robin@gmail.com>
2015-04-08 11:15:38 +0200

    Change finished games page.

---
+++ p/g/games-finished.c
@@ -0,0 +1,19 @@
+#include "html.h"
+header
+
+<h1>Games which I finished (recently)</h1>
+
+<p>List of games which I finished :)</p>
+<p>For some time I didn&apos;t play games... but it changed bacause steam, gog and Linux support.</br>
+So this list contains only games which I finished in "new era".</br>
+And yes.. some of them I play on my MacAIR running OS X.</p>
+
+
+<p>Alone In The Dark I - link(link,"http://www.gog.com/game/alone_in_the_dark")</p>
+<p>Leisure Suit Larry: Love for Sail! - link(link,"http://en.wikipedia.org/wiki/Leisure_Suit_Larry:_Love_for_Sail!")</p>
+<p>Leisure Suit Larry in the Land of the Lounge Lizards (EGA/VGA) - link(link, "http://en.wikipedia.org/wiki/Leisure_Suit_Larry_in_the_Land_of_the_Lounge_Lizards")</p>
+<p>Leisure Suit Larry 5: Passionate Patti Does a Little Undercover Work - link(link, "http://en.wikipedia.org/wiki/Leisure_Suit_Larry_5:_Passionate_Patti_Does_a_Little_Undercover_Work")</p>
+
+<p>Hotline Miami - link(link, "http://en.wikipedia.org/wiki/Hotline_Miami")</p>
+
+tail
