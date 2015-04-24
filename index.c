ac4fbe834f1833835bc0211cdc0defd04d3366e2
Robin Hack <hack.robin@gmail.com>
2015-04-24 12:46:36 +0200

    index: Add arduino hello world link.

--- index.c
+++ index.c
@@ -11,6 +11,7 @@ link(my github, "https://github.com/marmolak")
 <p>link(Hacking BlackThorne for fun, "/g/b/blackthorne.html")</p>
 <p>link(Games which I finished (recently), "/g/g/games-finished.html")</p>
 <p>link(Infamous (not much known) gcc bug, "/g/g/gcc-bug.html")</p>
+<p>link(My own first Arduino hello world, "/g/a/arduino-hello.html")</p>
 </div>
 <div>
 </div>
