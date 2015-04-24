d23edc10bf5a262fda8fd10209efa27aa52ba158
Robin Hack <hack.robin@gmail.com>
2015-04-24 12:39:41 +0200

    Change arduino-hello page.

---
+++ p/a/arduino-hello.c
@@ -0,0 +1,17 @@
+#include "html.h"
+header
+
+<h1>My own first Arduino hello world</h1>
+
+<p>I know that internet is full of arduino hello worlds these days. But this is mine!
+</p>
+
+<p>I looks ugly as hell because I just cut old UTP network cable to pieces and used them.</p>
+
+link(photo, "https://plus.google.com/photos/113048198898926446313/albums/6141246655859309425/6141246664083998226?banner=pwa&pid=6141246664083998226&oid=113048198898926446313")
+and
+link(code, "https://github.com/marmolak/arduino/blob/master/helloworld/hello.c")
+
+<p>You can find what it does in source code comment</p>
+
+tail
