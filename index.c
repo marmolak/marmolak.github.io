d4ad53c4df9cf6974c2925ec1927b845ec08eabd
Robin Hack <hack.robin@gmail.com>
2015-05-09 11:34:26 +0200

    index.html: Add link to BanaNAS - home made NAS solution

--- index.c
+++ index.c
@@ -12,6 +12,7 @@ link(my github, "https://github.com/marmolak")
 <p>link(Games which I finished (recently), "/g/g/games-finished.html")</p>
 <p>link(Infamous (not much known) gcc bug, "/g/g/gcc-bug.html")</p>
 <p>link(My own first Arduino hello world, "/g/a/arduino-hello.html")</p>
+<p>link(BanaNAS - home made NAS solution, "/g/b/bananas.html")</p>
 </div>
 <div>
 </div>
