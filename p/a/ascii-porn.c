59b9ad144535dc4582f1e2b45920f5a0da491b63
Robin Hack <hack.robin@gmail.com>
2015-06-25 12:01:36 +0200

    Change arduino-hello page.

---
+++ p/a/ascii-porn.c
@@ -0,0 +1,13 @@
+#include "html.h"
+header
+
+<h1>My first ascii art port (.plt.got on-fly patchin)</h1>
+
+<p>I found very interesting tool recently. It's named link(https:\/\/asciinema.org/, "https://asciinema.org/") and it's really cool.</p>
+
+<p>My first try to present something is "tadaa" here:<p>
+<p>
+<script type="text/javascript" src="https://asciinema.org/a/22436.js" id="asciicast-22436" async></script>
+</p>
+
+tail
