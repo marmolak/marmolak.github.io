9f9ba906bbc0e0cf6fee117dd7b755984b7b45bc
Robin Hack <hack.robin@gmail.com>
2015-11-26 10:14:05 +0100

    All g pages has been recopiled

--- p/g/games-finished.c
+++ p/g/games-finished.c
@@ -4,10 +4,9 @@ header
 <h1>Games which I finished (recently)</h1>
 
 <p>List of games which I finished :)</p>
-<p>For some time I didn&apos;t play games... but it changed bacause steam, gog and Linux support.</br>
-So this list contains only games which I finished in "new era".</br>
-And yes.. some of them I play on my MacAIR running OS X.</p>
-
+<p>For some time I didn&apos;t play games... but it changed because Steam, GoG with combination of Linux support and presence of my new fully loaded MacBook Pro.</br>
+So this list contains only games which I finished in the "new era".</br>
+</p>
 
 <p>Alone In The Dark I - link(link,"http://www.gog.com/game/alone_in_the_dark")</p>
 <p>Leisure Suit Larry: Love for Sail! - link(link,"http://en.wikipedia.org/wiki/Leisure_Suit_Larry:_Love_for_Sail!")</p>
