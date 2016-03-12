7a1a8cf4c7dbdaac722945abc3e2a49b5c0d8f14
Robin Hack <hack.robin@gmail.com>
2016-03-12 22:42:58 +0100

    New look of front page :)

--- include/html.h
+++ include/html.h
@@ -1,23 +1,53 @@
 #define header \
-<html> \
-<head> \
-<title>Marmolak (not so sweet) home</title> \
+<!doctype html>	\
+<html>	\
+<head>	\
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> \
+<title>Marmolak&#39;s (not so sweet) home</title> \
+<link rel="stylesheet" href="styles.css" type="text/css" /> \
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" /> \
 </head> \
 <body> \
-<link rel="stylesheet" type="text/css" href="/marmolak.css"> \
-<div id="main-body"> \
-<div> \
-<a href="/"><img src="/img/logo.png"></a> \
-</div> \
+<div id="container"> \
+    <header> \
+	<div class="width"> \
+    		<h1><a href="/">Marmolak&#39;s place</a></h1> \
+	    	<nav> \
+    			<ul> \
+        			<li class="start selected"><a href="index.html">Home</a></li> \
+         	   		<li><a href="https://github.com/marmolak">My github</a></li> \
+          	 		<li class="end"><a href="#">Contact</a></li> \
+        		</ul> \
+    		</nav> \
+       	</div> \
+    </header> \
+    <div id="body" class="width"> \
+		<section id="content" class="two-column with-right-sidebar"> \
+	    <article> \
+
 
 /* End of page */
 #define tail \
-<a href="/">Back to main page</a>
-</div> \
-</body> \
-</html>
-
-#define tail_front \
+		</article> \
+        </section> \
+        <aside class="sidebar big-sidebar right-sidebar"> \
+            <ul> \
+               <li> \
+                    <h4>Blocklist</h4> \
+                    <ul class="blocklist"> \
+                        <li><a class="selected" href="index.html">Home Page</a></li> \
+<li>link(Hacking BlackThorne for fun, "/g/b/blackthorne.html")</li> \
+<li>link(Games which I finished (recently), "/g/g/games-finished.html")</li> \
+<li>link(Infamous (not much known) gcc bug, "/g/g/gcc-bug.html")</li> \
+<li>link(My own first Arduino hello world, "/g/a/arduino-hello.html")</li> \
+<li>link(BanaNAS - home made NAS solution, "/g/b/bananas.html")</li> \
+<li>link(My first ascii art porn (.got.plt on-the-fly patching), "/g/a/ascii-porn.html")</li> \
+                    </ul> \
+                </li> \
+            </ul> \
+        </aside> \
+    	<div class="clear"></div> \
+    </div> \
 </div> \
 </body> \
 </html>
--- index.c
+++ index.c
@@ -1,21 +1,8 @@
 #include <html.h>
 header
 
-<div>
-<p>
-link(my github, "https://github.com/marmolak")
-</p>
-</div>
+<h2>Intro</h2>
 
-<div>
-<p>link(Hacking BlackThorne for fun, "/g/b/blackthorne.html")</p>
-<p>link(Games which I finished (recently), "/g/g/games-finished.html")</p>
-<p>link(Infamous (not much known) gcc bug, "/g/g/gcc-bug.html")</p>
-<p>link(My own first Arduino hello world, "/g/a/arduino-hello.html")</p>
-<p>link(BanaNAS - home made NAS solution, "/g/b/bananas.html")</p>
-<p>link(My first ascii art porn (.got.plt on-the-fly patching), "/g/a/ascii-porn.html")</p>
-</div>
-<div>
-</div>
+Hello bacmk!
 
-tail_front
+tail
