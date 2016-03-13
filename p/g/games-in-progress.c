6fbd7dc75133029beb0757019a1a0bcd95471ca6
Robin Hack <hack.robin@gmail.com>
2016-03-13 16:40:21 +0100

    All g pages has been recopiled

---
+++ p/g/games-in-progress.c
@@ -0,0 +1,18 @@
+#include "html.h"
+header
+
+<h2>Gameplay in progress</h2>
+
+<p>Sometimes I have some games in queue and I play them one by one.
+Sometimes I have more games in progress in parallel :).
+</p>
+
+<p>
+<ul>
+	<li>Shadow Warrior 2013</li>
+	<li>Legend of Grimrock</li>
+	<li>Doom 1 (Doomsday Engine)
+</ul>
+</p>
+
+tail
